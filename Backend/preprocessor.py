d9dca7b2aafa68d175c45498c6dfc1c19c9d8fa1
Nour-saber57 <noormohmed584@gmail.com>
2026-06-20 01:16:44 +0300

    change the variables names to match all the files

--- Backend/preprocessor.py
+++ Backend/preprocessor.py
@@ -49,10 +49,10 @@ def split_data(df, target, test_size=0.2, random_state=42):
 
 def scaling(X_train, X_test):
     scaler = StandardScaler()
-    X_train_scaled = scaler.fit_transform(X_train)
-    X_test_scaled = scaler.transform(X_test)
+    X_train = scaler.fit_transform(X_train)
+    X_test= scaler.transform(X_test)
 
-    return X_train_scaled, X_test_scaled
+    return X_train, X_test
 
 
 
