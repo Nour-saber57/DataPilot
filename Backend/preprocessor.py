8d69b839523584049ffc44d8cec19189fe5b431a
Nour-saber57 <noormohmed584@gmail.com>
2026-06-20 01:35:18 +0300

    Delete the code

--- Backend/preprocessor.py
+++ Backend/preprocessor.py
@@ -16,16 +16,7 @@ def identify_feature_types(df, target):
 
     return numeric_columns, categorical_columns
 
-def split_data(df, target, test_size=0.2, random_state=42):
-    df = df.dropna(subset=[target])
-    X = df.drop(columns=[target])
-    y = df[target]
 
-    X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=test_size, random_state=random_state
-    )
-
-    return X_train, X_test, y_train, y_test
 
 
 def create_preprocessing_pipeline(df, target):
@@ -34,6 +25,7 @@ def create_preprocessing_pipeline(df, target):
     
     numeric_pipeline = Pipeline(steps=[
         ('imputer', SimpleImputer(strategy='median', add_indicator=True)),
+        ('scaler', StandardScaler())
     ])
 
     
@@ -49,14 +41,18 @@ def create_preprocessing_pipeline(df, target):
 
     return pipeline 
 
+def split_data(df, target, test_size=0.2, random_state=42):
+    df = df.dropna(subset=[target])
+    X = df.drop(columns=[target])
+    y = df[target]
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=test_size, random_state=random_state
+    )
+
+    return X_train, X_test, y_train, y_test
 
 
-
-    scaler = StandardScaler()
-    X_train = scaler.fit_transform(X_train)
-    X_test= scaler.transform(X_test)
-
-    return X_train, X_test
 
 
 
